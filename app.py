05f1fdbfa0d8a8c3d7a58606a1110d4c95ac503d
Jasiu2028@gmail.com <2028Jasiu>
2022-09-08 19:18:42 +0200

    Płatność-początek

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, request
+import stripe as stripe
+from flask import Flask, render_template, redirect, url_for, request, abort
 import sqlalchemy
 from streamlit import form
 
 app = Flask(__name__)
 
+app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51LfnIsEppafJpnA12mXgW9hAjYCeZdhwmgUMHtHaSROmeOoiAUDSzu52Es5VLowz70WZLcm49jWExfn34GEuvl690064rVJpQa'
+app.config['STRIPE_SECRET_KEY'] = 'sk_test_51LfnIsEppafJpnA1yhI6Es3uPRQgb4Vl1YcnTLjObPt3cAnU35oDKh08unvdR09YpdiWQJkzGkyk5pKPS6XvlHlP00c8TRbQdf'
+
+stripe.api_key = app.config['STRIPE_SECRET_KEY']
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -42,6 +47,24 @@ def seatsDjango():
 @app.route("/seanseBulletTrain/seats", methods=["POST", "GET"])
 def seatsBulletTrain():
     return render_template("seats.html")
+
+@app.route("/payment")
+def payment():
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price': 'price_1LfndoEppafJpnA1v2Tm3qcL',
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url=url_for('payment', _external=True),
+        )
+        return render_template(
+            'home.html',
+            checkout_session_id = session['id'],
+            checkout_public_key = app.config['STRIPE_PUBLIC_KEY']
+            )
 
 
 if __name__ == '__main__':
