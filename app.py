b944a7134af801a59e705b43a4333fd42bc45391
Jan Słaby <Jasiu2028@gmail.com>
2022-09-11 14:10:19 +0200

    PayPal

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import stripe as stripe
-from flask import Flask, render_template, redirect, url_for, request, abort
+import paypalrestsdk
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 import sqlalchemy
 from streamlit import form
 
 app = Flask(__name__)
 
-app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51LfnIsEppafJpnA12mXgW9hAjYCeZdhwmgUMHtHaSROmeOoiAUDSzu52Es5VLowz70WZLcm49jWExfn34GEuvl690064rVJpQa'
-app.config['STRIPE_SECRET_KEY'] = 'sk_test_51LfnIsEppafJpnA1yhI6Es3uPRQgb4Vl1YcnTLjObPt3cAnU35oDKh08unvdR09YpdiWQJkzGkyk5pKPS6XvlHlP00c8TRbQdf'
+paypalrestsdk.configure({
+  "mode": "sandbox", # sandbox or live
+  "client_id": "AS8Ip1U8_Eh_9iHTvtCIBg3O8QhwbMeWlLuQg1vpfa08aNALHOV4JaMvs18wGIYmMXFOB2ROEGHJgdFo",
+  "client_secret": "EK0L-coYgnXh54r44rydlEIBFDjkeom1SJeS3hPgsoN_9LXuUCFOEMcCOmIiDUrS3QVIoURrdCdWYPxW" })
 
-stripe.api_key = app.config['STRIPE_SECRET_KEY']
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -48,24 +49,56 @@ def seatsDjango():
 def seatsBulletTrain():
     return render_template("seats.html")
 
-@app.route("/payment")
+@app.route('/payment', methods=['POST'])
 def payment():
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-                'price': 'price_1LfndoEppafJpnA1v2Tm3qcL',
-                'quantity': 1,
-            }],
-            mode='payment',
-            success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url=url_for('payment', _external=True),
-        )
-        return render_template(
-            'home.html',
-            checkout_session_id = session['id'],
-            checkout_public_key = app.config['STRIPE_PUBLIC_KEY']
-            )
 
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"},
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/payment/execute",
+            "cancel_url": "http://localhost:3000/"},
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "bilet ulgowy",
+                    "sku": "12345",
+                    "price": "12.50",
+                    "currency": "PLN",
+                    "quantity": 1},
+                {
+                    "name": "bilet normanly",
+                    "sku": "12345",
+                    "price": "17.50",
+                    "currency": "PLN",
+                    "quantity": 1}],
+            },
+            "amount": {
+                "total": "30.00",
+                "currency": "PLN"},
+            "description": "Zakup biletu do kina."}]})
+
+    if payment.create():
+        print('Payment success!')
+    else:
+        print(payment.error)
+
+    return jsonify({'paymentID' : payment.id})
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    success = False
+
+    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+
+    if payment.execute({'payer_id' : request.form['payerID']}):
+        print('Execute success!')
+        success = True
+    else:
+        print(payment.error)
+
+    return jsonify({'success' : success})
 
 if __name__ == '__main__':
     app.debug=True
